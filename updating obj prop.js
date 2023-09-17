b7ed01f7c626232331a022ae9b910b7ab5bde2bd
Bernadette <bernadettemetran35@gmail.com>
2023-09-16 22:51:02 -0700

    first commit

---
+++ updating obj prop.js
@@ -0,0 +1,62 @@
+// Updating Object Properties 
+
+
+<!-- Update the myDog object's name property. 
+    Change her name from Coder to Happy Coder -->
+
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.name = "Happy Coder";
+console.log(myDog);
+
+//console output :
+{ name: 'Happy Coder',
+  legs: 4,
+  tails: 1,
+  friends: [ 'freeCodeCamp Campers' ] }
+
+  <!-- **********-->
+//Add New Properties to a JS Object
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ['freeCodeCamp Campers']
+};
+
+myDog.bark = "woof";
+
+console.log(myDog);
+//console output:
+{ name: 'Happy Coder',
+  legs: 4,
+  tails: 1,
+  friends: [ 'freeCodeCamp Campers' ],
+  bark: 'woof' }
+
+<!---***********-->
+  //Delete Properties from a JS Object
+
+  const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+
+  delete myDog.tails;
+
+  console.log(myDog);
+
+  //console output:
+  { name: 'Happy Coder',
+  legs: 4,
+  friends: [ 'freeCodeCamp Campers' ],
+  bark: 'woof' }
